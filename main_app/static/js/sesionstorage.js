d2f14d926119b983259be7955d994625ea82bbed
Matiasbcg <145684006+Matiasbcg@users.noreply.github.com>
2025-04-17 17:00:26 -0400

    Delete main_app directory

--- main_app/static/js/sesionstorage.js
+++
@@ -1,5 +0,0 @@
-const nombre = sessionStorage.getItem('cliente_nombre');
-if (nombre) {
-    console.log("Cliente en sesión:", nombre);
-    document.getElementById("saludo").innerText = `Bienvenido, ${nombre}`;
-}
